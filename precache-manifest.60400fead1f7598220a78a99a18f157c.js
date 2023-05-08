8839b3b038c3a50015da16cc0d9cb4f7cfc0a7fd
sohaibasaad <46029942+sohaibasaad@users.noreply.github.com>
2023-05-08 15:07:48 +0500

    Updates

---
+++ precache-manifest.60400fead1f7598220a78a99a18f157c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe7052c764232f105bb",
+    "url": "/react-course/static/css/main.b4f37a06.chunk.css"
+  },
+  {
+    "revision": "ffe7052c764232f105bb",
+    "url": "/react-course/static/js/main.c8446711.chunk.js"
+  },
+  {
+    "revision": "ca1346b2c639586d8fa8",
+    "url": "/react-course/static/js/runtime~main.e1832b59.js"
+  },
+  {
+    "revision": "831d4a245076f5bff701",
+    "url": "/react-course/static/js/2.953ec56f.chunk.js"
+  },
+  {
+    "revision": "a666a093997e37f4e0b007ac77c657fc",
+    "url": "/react-course/static/media/bird.a666a093.svg"
+  },
+  {
+    "revision": "a4ecaf5fe64129bca06c5dbf2b350744",
+    "url": "/react-course/static/media/cat.a4ecaf5f.svg"
+  },
+  {
+    "revision": "d2066a8abe4a570f2e950cc249abd4d7",
+    "url": "/react-course/static/media/cow.d2066a8a.svg"
+  },
+  {
+    "revision": "52e6bfb7ce14438c484c518f045d0155",
+    "url": "/react-course/static/media/dog.52e6bfb7.svg"
+  },
+  {
+    "revision": "ead92468a9347e3c478ec0bf52e0c22d",
+    "url": "/react-course/static/media/gator.ead92468.svg"
+  },
+  {
+    "revision": "38abe518abe374db95433cdb03be84f8",
+    "url": "/react-course/static/media/heart.38abe518.svg"
+  },
+  {
+    "revision": "05a990adafcebd79b81d32d86215a89d",
+    "url": "/react-course/static/media/horse.05a990ad.svg"
+  },
+  {
+    "revision": "bbde6e98f7016ac5b823c2e1d46f160d",
+    "url": "/react-course/index.html"
+  }
+];
